9a8ee21c948772e11a83926777b9d74ae3660663
Kolinko Roman <118218201+Kolinko-Roman@users.noreply.github.com>
2024-12-07 13:16:18 +0200

    Update Lab12.cpp

--- Lab12.cpp
+++ Lab12.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Appliance {
+protected:
+    int power;
+public:
+    Appliance(int power) : power(power) {}
+    void turnOn() const {
+        cout << "Прилад увімкнено. Потужність: " << power << " Вт." << endl;
+    }
+    int getPower() const {
+        return power;
+    }
+};
+class WashingMachine : public Appliance {
+private:
+    int capacity;
+public:
+    WashingMachine(int power, int capacity) : Appliance(power), capacity(capacity) {}
+    void startWash() const {
+        cout << "Прання розпочато. Ємність: " << capacity << " кг." << endl;
+    }
+    int getCapacity() const {
+        return capacity;
+    }
+};
+int main() {
+    Appliance appliance(1500);
+    appliance.turnOn();
+    WashingMachine wm(2000, 7);
+    wm.turnOn();
+    wm.startWash();
+
+    return 0;
+}
 
